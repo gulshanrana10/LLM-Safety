2eac9d09935f8f8bbd9ccfe0ff29e1f9a9a65995
MOUHIB Otman <mouhib_otm@upf.ac.ma>
2024-09-04 18:18:24 +0000

    FIX 2

--- src/backend/anonymizers/default_anonymizer.py
+++ src/backend/anonymizers/default_anonymizer.py
@@ -11,14 +11,14 @@ class DefaultAnonymizer(Anonymizer):
         super().__init__(conf_file=conf_file, models_file=models_file, entities=entities)
     
     def _get_nlp_configuration(self):
+        # No configuration needed for DefaultAnonymizer
         pass
-    def do_anonymize(self, texts):
+
+    def do_anonymize(self, texts, true_annotations=None):
         analyzer = AnalyzerEngine()
         
         if isinstance(texts, list):
-            results = []
-            for text in texts:
-                results.append(analyzer.analyze(text=text, entities=self.entities, language="en"))
+            results = [analyzer.analyze(text=text, entities=self.entities, language="en") for text in texts]
         else:
             results = analyzer.analyze(text=texts, entities=self.entities, language="en")
         
@@ -26,5 +26,9 @@ class DefaultAnonymizer(Anonymizer):
         print(f"                 Default List                      ")
         print(f"***************************************************")
         anonymized_texts = self._anonymize(texts, results)
+
+        # If true annotations are provided, evaluate the anonymization
+        if true_annotations:
+            self.evaluate_anonymization(results, true_annotations)
 
         return anonymized_texts
